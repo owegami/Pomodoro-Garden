15657202cfef4e007509638a087f58ecc52ceeb7
owegami <busimeiru@gmail.com>
2020-10-31 14:35:42 -0700

    "Fix basic rendering of react app"

--- server/router.js
+++ server/router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
-app.use(express.static(__dirname + '/public/index.html'));
+app.use('/',express.static(__dirname + '/../public'));
 
 app.get('/', (req, res) => {
   res.send("I'm hacking the mainframe!");
