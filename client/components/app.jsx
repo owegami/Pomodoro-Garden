cbb3996aaa6117b3faad1443332afce7cb83619d
owegami <busimeiru@gmail.com>
2020-10-31 14:43:59 -0700

    "Change name of lapsedVisual"

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import TimerVisual from './timer.jsx';
-import LapsedVisual from './lapsedVisual.jsx';
+import Visualizer from './visualizer.jsx';
 
 const App = () => {
   //declare state here when we want some
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <TimerVisual />
-      <LapsedVisual />
+      <Visualizer />
     </div>
   )
 }
