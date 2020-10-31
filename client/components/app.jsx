6fa51b10ef38f32b2e3433c9c3de48fe4a35073b
owegami <busimeiru@gmail.com>
2020-10-31 13:10:28 -0700

    n building basic react client"

---
+++ client/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  //declare state here when we want some
+
+  return (
+    <div>
+      <span>Basic div and span rendering</span>
+    </div>
+  )
+}
+
+export default App;
