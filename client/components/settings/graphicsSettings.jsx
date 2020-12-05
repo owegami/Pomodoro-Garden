deb4064d907f0ac9b7924a99d5e54cbe87b5cee4
owegami <busimeiru@gmail.com>
2020-12-05 07:54:55 -0800

    "Fix radio buttons to allow for dynamic user selection of one art style"

--- client/components/settings/graphicsSettings.jsx
+++ client/components/settings/graphicsSettings.jsx
@@ -3,25 +3,31 @@ import styled from 'styled-components';
 
 import {CurrentSettingsForm, SettingsQuestion, Button} from './../../view/styledComponents.jsx';
 
-const GraphicsSettings = (selectHighContrast, setSelectHighContrast) => {
-  console.log(selectHighContrast);
+const GraphicsSettings = ({selectHighContrast, setSelectHighContrast}) => {
+
+  console.log(selectHighContrast)
   return (
     <CurrentSettingsForm onSubmit={(event) => {
       event.preventDefault();
+      console.log('event.target.value', event.target.value);
       setSelectHighContrast(event.target.value);
       }}>
 
       <SettingsQuestion>
+    Current preference: {selectHighContrast === '' ? 'Low contrast art' : 'High contrast art'} <br/>
           Select which image you prefer: <br/>
         <label>
+          <input type='radio' name='LowContrast' value='' checked={selectHighContrast === ''}  onChange={(event) => {
+            setSelectHighContrast(event.target.value);
+          }} />
           <img src='/images/Tomato_5.png' alt='Low contrast picture of tomato plant'/>
-          <input type="radio" value=''></input>
         </label>
         <label>
+          <input type='radio' name='HighContrast' value='HighContrast' checked={selectHighContrast === 'HighContrast'} onChange={(event) => {
+            setSelectHighContrast(event.target.value);
+          }} />
           <img src='/imagesHighContrast/Tomato_5.png' alt='[PLACEHOLDER IMAGE] High contrast picture of tomato plant'/>
-          <input type="radio" value='HighContrast'></input>
         </label>
-        <Button>I want this level of contrast when possible</Button>
       </SettingsQuestion>
     </CurrentSettingsForm>
   )
