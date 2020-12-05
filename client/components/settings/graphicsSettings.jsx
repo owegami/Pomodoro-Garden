8e0f02d9904fc48bd38849019fa472622c762906
owegami <busimeiru@gmail.com>
2020-12-04 18:00:26 -0800

    "Add placeholder image for high contrast question"

--- client/components/settings/graphicsSettings.jsx
+++ client/components/settings/graphicsSettings.jsx
@@ -3,17 +3,19 @@ import styled from 'styled-components';
 
 import {CurrentSettingsForm, SettingsQuestion, Button} from './../../view/styledComponents.jsx';
 
-import regularimage from './../../../public/images/Tomato_5.png';
-
 const GraphicsSettings = (selectHighContrast, setSelectHighContrast) => {
 
   return (
-    <CurrentSettingsForm>
+    <CurrentSettingsForm onClick={() => {setSelectHighContrast()}}>
       <SettingsQuestion>
           Select which image you prefer: <br/>
         <label>
           <img src='/images/Tomato_5.png' alt='Low contrast picture of tomato plant'/>
-          <input type="radio" value={regularimage}></input>
+          <input type="radio" value=''></input>
+        </label>
+        <label>
+          <img src='/imagesHighContrast/Tomato_5.png' alt='[PLACEHOLDER IMAGE] High contrast picture of tomato plant'/>
+          <input type="radio" value='HighContrast'></input>
         </label>
       </SettingsQuestion>
     </CurrentSettingsForm>
