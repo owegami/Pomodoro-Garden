6208205998555eabd3063a51ffca3a0adf5b24aa
owegami <busimeiru@gmail.com>
2020-12-05 08:44:53 -0800

    "Add clock tick sounds"

--- public/sounds.js
+++ public/sounds.js
@@ -1,6 +1,6 @@
 exports.chimes1 = function() {
   this.sound = document.createElement('audio');
-  this.sound.src = '/30606__acclivity__jamesportwindchimes.mp3';
+  this.sound.src = '/sounds/30606__acclivity__jamesportwindchimes.mp3';
   this.sound.setAttribute('preload', 'auto');
   this.sound.setAttribute('controls', 'none');
   this.sound.style.display = 'none';
@@ -15,7 +15,112 @@ exports.chimes1 = function() {
 
 exports.chimes2 = function() {
   this.sound = document.createElement('audio');
-  this.sound.src = '/424033__mandymatz__wind-chimes.mp3';
+  this.sound.src = '/sounds/424033__mandymatz__wind-chimes.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock1 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick1.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock2 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick2.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock3 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick3.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock4 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick4.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock5 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick5.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock6 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick6.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock7 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick7.m4a';
   this.sound.setAttribute('preload', 'auto');
   this.sound.setAttribute('controls', 'none');
   this.sound.style.display = 'none';
