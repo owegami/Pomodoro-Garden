7be7fc509a10012a320a728a740584ab31540a17
owegami <busimeiru@gmail.com>
2020-12-21 15:19:50 -0800

    "Add redux dev tools to store"

--- client/store/store.js
+++ client/store/store.js
@@ -1,8 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import initialState from './initialState.js';
 import rootReducer from './../reducers/rootReducer.js';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+  );
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store has dispatched:', store.getState());
