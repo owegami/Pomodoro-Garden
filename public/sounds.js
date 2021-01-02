1ccf7c0957447a61264643eb3b521ccc097f991e
owegami <busimeiru@gmail.com>
2021-01-01 20:14:41 -0800

    "Add new sounds and sound files for customization"

--- public/sounds.js
+++ public/sounds.js
@@ -132,3 +132,498 @@ exports.clock7 = function() {
     this.sound.pause()
   }
 }
+
+exports.aTone = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/aTone.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.bellAlert = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/bellAlert.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.bellsIn = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/bellsIn.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.bellsOut = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/bellsOut.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.boxingBell = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/boxingBell.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.britMaleBreak = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/britMaleBreak.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.britMaleStart = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/britMaleStart.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.britMaleThree = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/britMaleThree.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.cardinalBird = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/cardinalBird.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.computerMagic = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/computerMagic.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.cuckooClock = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/cuckooClock.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.fairyBellIn = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/fairyBellIn.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.fairyBellOut = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/fairyBellOut.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.fionaBreak = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/fionaBreak.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.fionaStart = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/fionaStart.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.fionaThree = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/fionaThree.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.gong = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/gong.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.insistantBell = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/insistantBell.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.maginWand = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/magicWand.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.musicBox = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/musicBox.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.rumbleOut = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/rumbleOut.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.serviceBell = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/serviceBell.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.shipBell = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/shipBell.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.stepIn = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/stepIn.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.stepOut = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/stepOut.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.thinkingChimes = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/thinkingChimes.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.clock7 = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/clockTick7.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.tinBell = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/tinBell.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.turkeyPurr = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/turkeyPurr.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.warblingVireo = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/warblingVireo.mp3';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.wobbleIn = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/wobbleIn.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.wobbleOut = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/wobbleOut.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
+
+exports.woodenChimeEcho = function() {
+  this.sound = document.createElement('audio');
+  this.sound.src = '/sounds/woodenChimeEcho.m4a';
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+  }
+  this.stop = function() {
+    this.sound.pause()
+  }
+}
